2da76112e651016afb72fbb295c8ca94c2231654
Radvila <radvila.simkus@gmail.com>
2017-12-14 19:55:01 +0200

    SQL and some UI

--- src/com/company/UI.java
+++ src/com/company/UI.java
@@ -1,4 +1,50 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class UI {
+
+    public int pasirinkimas = 1;
+    public String id;
+    public String vardas;
+    public String pavarde;
+    public String pareigos;
+    public String tipas;
+    public String valstybinisNr;
+    public String pavadinimas;
+    public String tiekiaIKI;
+    public int kaina;
+    public int kodas;
+    public int kvadratura;
+    public int nr;
+
+    public void start() {
+        SQL db = new SQL();
+        BufferedReader bufRead = new BufferedReader(new InputStreamReader(System.in));
+
+        //atspausdinti pasirinkimus
+
+        while (pasirinkimas != 0) {
+            try {
+                System.out.print(">");
+                pasirinkimas = Integer.parseInt(bufRead.readLine());
+                switch (pasirinkimas) {
+                    case 0:
+                        break;
+                    case 1:
+
+                        break;
+                    default:
+                        System.out.println("Blogas pasirinkimas");
+                        break;
+                }
+            } catch (IOException e) {
+                System.out.println("Klaida skaitant ivesti");
+            } catch (NumberFormatException e) {
+                System.out.println("Netinkamas ivesties formatas");
+            }
+        }
+    }
 }
